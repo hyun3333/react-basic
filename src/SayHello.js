67feab92d258492850fb2a8b70f4e7969e69ac0b
hyun <hyun231366@gmail.com>
2023-06-12 13:07:13 +0900

    JSX문법

---
+++ src/SayHello.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// 컴포넌트
+// 컴포넌트 파일은 대문자로 작성하는 것이 관례입니다.
+function SayHello() {
+    return (
+        <div>
+            <p>404</p>
+            <span>405</span>
+            <p>500</p>
+            <p>Null</p>
+            <p>NaN</p>            
+        </div>
+    );
+}
+
+export default SayHello;

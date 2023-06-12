48902774a329e64d5a3112f77d826585d8ed3e18
hyun <hyun231366@gmail.com>
2023-06-12 16:54:48 +0900

    props 활용

--- src/component/chap02_props/FoodItem.js
+++ src/component/chap02_props/FoodItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const FoodItem = (props) => {
+const FoodItem = ({foodId: id, foodName: fName, price}) => {
 
-    console.log('props' , props);
+    // console.log('props' , props);
   return (
-    <li id={props.foodId}>{props.foodName} ({props.price}원)</li>
+    <li id={id}>{fName} ({price}원)</li>
   );
 }
 

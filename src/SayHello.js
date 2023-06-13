5a759a3ba7d885177d0e18c1c8ef4d6b7d62b7d8
hyun <hyun231366@gmail.com>
2023-06-13 10:15:00 +0900

    props end

--- src/SayHello.js
+++ src/SayHello.js
@@ -2,10 +2,11 @@ import React from "react";
 
 // 컴포넌트
 // 컴포넌트 파일은 대문자로 작성하는 것이 관례입니다.
-function SayHello() {
+function SayHello(props) {
     return (
         <div>
             <p>404</p>
+            {props.children}
             <span>405</span>
             <p>500</p>
             <p>Null</p>
